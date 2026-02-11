35e624a658c34f3eabdb80ee9168ef74b9cb29cb
arm3nta <arma2002@hotmail.com>
2026-02-11 12:46:32 -0600

    se agrego static para favicon

--- app.py
+++ app.py
@@ -11,7 +11,8 @@ import threading
 APP_SECRET = os.environ.get("APP_SECRET", "dev-secret-change-me")
 DB_PATH = os.path.join(os.path.dirname(__file__), 'app.db')
 
-app = Flask(__name__, template_folder='templates')
+
+app = Flask(__name__, template_folder='templates', static_folder='static')
 app.secret_key = APP_SECRET
 
 
@@ -2656,3 +2657,4 @@ import os
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
